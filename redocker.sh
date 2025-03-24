91e8ea1e2ddbcf2696c530333bd7783df20479e2
Ross Llewallyn <rossllewallyn@proton.me>
2025-03-24 04:37:03 -0400

    accept OpenAI key in docker build

--- redocker.sh
+++ redocker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Open AI API key required"
+  exit 1
+fi
+
+OPENAI_API_KEY="$1"
+
 IMAGE_NAME=fraction.work
 
 echo "Building new Docker image (if repo changed, is it pushed and CACHEBUST updated?)..."
@@ -11,7 +18,7 @@ echo
 echo "Building new Docker container from image..."
 echo
 
-docker run -d -p 3306:3306 -p 5000:5000 -p 3000:3000 $IMAGE_NAME
+docker run -d -p 3306:3306 -p 5000:5000 -p 3000:3000 -e OPENAI_API_KEY=$OPENAI_API_KEY $IMAGE_NAME
 
 echo
 echo "Redocking complete!"
