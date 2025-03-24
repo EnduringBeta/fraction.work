e76ca62f3760d2ea2efed481f061b28969f16966
Ross Llewallyn <rossllewallyn@proton.me>
2025-03-24 05:04:54 -0400

    stray quote nice

--- api/app.py
+++ api/app.py
@@ -429,7 +429,7 @@ def get_description(player_id):
         cursor = conn.cursor(dictionary=True)
         query = f"""
             SELECT player_name, games, batting_average, rbi, slugging_percent, position
-            FROM {table_players} WHERE id = %s"
+            FROM {table_players} WHERE id = %s
         """
         cursor.execute(query, (player_id,))
         player = cursor.fetchone()
